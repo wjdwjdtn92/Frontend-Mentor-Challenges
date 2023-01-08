89195fe54e9d6cac4d58f422b1bc5fa2c9f9374e
Jung Su <wjdwjdtn92@naver.com>
2023-01-08 13:57:51 +0900

    feat: mobile css 구현

--- faq-accordion-card-main/src/App.js
+++ faq-accordion-card-main/src/App.js
@@ -1,44 +1,21 @@
-import Footer from "./components/Footer.js";
-import Main from "./components/Main.js";
-
-const DUMY_DATA = [
-  {
-    title: "How many team members can I invite?",
-    content:
-      "You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.",
-  },
-  {
-    title: "What is the maximum file upload size?",
-    content:
-      "No more than 2GB. All files in your account must fit your allotted storage space.",
-  },
-  {
-    title: "How do I reset my password?",
-    content:
-      "Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.",
-  },
-  {
-    title: "Can I cancel my subscription?",
-    content:
-      "Yes! Send us a message and we’ll process your request no questions asked.",
-  },
-  {
-    title: "Do you provide additional support?",
-    content:
-      "Chat and email support is available 24/7. Phone lines are open during normal business hours.",
-  },
-];
+import Footer from "./components/Layout/Footer.js";
+import Header from "./components/Layout/Header.js";
+import Main from "./components/Layout/Main.js";
 
 export default function App({ $target }) {
+  const $wrapper = document.createElement("div");
+  $wrapper.classList = "wrapper";
+  $target.append($wrapper);
+
+  const header = new Header({
+    $target: $wrapper,
+  });
+
   const main = new Main({
-    $target,
-    initialState: {
-      title: "FAQ",
-      data: DUMY_DATA,
-    },
+    $target: $wrapper,
   });
 
   const footer = new Footer({
-    $target,
+    $target: $wrapper,
   });
 }
